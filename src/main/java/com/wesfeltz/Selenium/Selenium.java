8a57995e18d5ed92e84e458b60d02a5493dcbfa1
Wes Feltz <wesfeltz@gmail.com>
2025-02-12 23:07:48 -0500

    initial

---
+++ src/main/java/com/wesfeltz/Selenium/Selenium.java
@@ -0,0 +1,10 @@
+package com.wesfeltz.Selenium;
+
+/**
+ * Hello world!
+ */
+public class Selenium {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
