8db0412da8324ed163cd6b867faeb0394e39161f
Wes Feltz <wesfeltz@gmail.com>
2025-02-13 10:15:39 -0500

    added comments

--- src/main/java/com/wesfeltz/Selenium/SeleniumSuite.java
+++ src/main/java/com/wesfeltz/Selenium/SeleniumSuite.java
@@ -12,24 +12,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumSuite {
     public static void main(String[] args) {
+    	//Manage chromedriver version and settings
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         try {
+        	//navigate to apnews
             driver.get("https://www.apnews.com");
             Thread.sleep(3000);
+            //get all header text, but then only use ones that are 5+ words in length
             List<WebElement> articles = driver.findElements(By.cssSelector("h2, h3"));
-            
             List<WebElement> validArticles = articles.stream().filter(e -> e.getText().trim().split("\\s+").length >= 5)
             		.collect(Collectors.toList());
             
+            //if driver found article headers, state us a random one in the phrase of a question
             if (!articles.isEmpty()) {
             	Random random = new Random();
             	WebElement randomArticle = articles.get(random.nextInt(articles.size()));
